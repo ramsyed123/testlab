da2537035c398b88e6d499a08cf249642dae89dc
ramsyed123 <rameez786cool@gmail.com>
2024-11-24 16:30:47 +0530

    sss

--- terraform/V1-EC2.tf
+++ terraform/V1-EC2.tf
@@ -2,6 +2,7 @@ provider "aws" {
   region = "us-east-1"
 }
 
+
 resource "aws_instance" "demo-server" {
   ami           = "ami-0866a3c8686eaeeba"
   instance_type = "t2.micro"
@@ -9,5 +10,16 @@ resource "aws_instance" "demo-server" {
   subnet_id     = "subnet-03e8e04cfd51b7fb3"
   tags = {
     Name = "demo-server"
+  }
+}
+
+
+resource "aws_instance" "demo-server2" {
+  ami           = "ami-0866a3c8686eaeeba"
+  instance_type = "t2.micro"
+  key_name      = "lab"
+  subnet_id     = "subnet-03e8e04cfd51b7fb3"
+  tags = {
+    Name = "demo-server2"
   }
 }
